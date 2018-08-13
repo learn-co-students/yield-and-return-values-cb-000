5aba206f3c71448a5b503100309175f46241314c
Mitesh Patel <mit06@hotmail.com>
2018-08-13 20:02:03 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def hello(array)
   i = 0
-  while i < array.length
-    yield(array[i])
+  collection = []
+  while n < array.length
+    collection << yield(array[n])
     i += 1
   end
+  collection
 end
 
 
